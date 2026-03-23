bb6dd3b67038d2a59918c1c769c1779d96a38632
haeun92e0 <gracekim8729@gmail.com>
2026-03-23 15:42:52 +0900

    Feat: 자동차 목록을 관리하는 컬렉션 Cars 추가

---
+++ src/main/java/Model/Cars.java
@@ -0,0 +1,54 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Cars {
+    private final List<Car> cars;
+
+    public Cars(List<String> carNames) {
+        this.cars = createCars(carNames);
+    }
+
+    private List<Car> createCars(List<String> carNames) {
+        List<Car> initCars = new ArrayList<>();
+        for (String name : carNames) {
+            initCars.add(new Car(name.trim()));
+        }
+        return initCars;
+    }
+
+    public void moveAll(NumberGenerator generator) {
+        for (Car car : cars) {
+            car.move(generator.generate());
+        }
+    }
+
+    public List<Car> findWinners() {
+        int maxPosition = getMaxPosition();
+        List<Car> winners = new ArrayList<>();
+        for (Car car : cars) {
+            addIfWinner(winners, car, maxPosition);
+        }
+        return winners;
+    }
+
+    private int getMaxPosition() {
+        int max = 0;
+        for (Car car : cars) {
+            max = Math.max(max, car.getPosition());
+        }
+        return max;
+    }
+
+    private void addIfWinner(List<Car> winners, Car car, int maxPosition) {
+        if (car.isAtPosition(maxPosition)) {
+            winners.add(car);
+        }
+    }
+
+    public List<Car> getCars() {
+        return Collections.unmodifiableList(cars); // 외부에서 조작하지 못하도록 방어
+    }
+}
