6d522cf30a3f98293d387ffa1cae9a5516c01225
haeun92e0 <gracekim8729@gmail.com>
2026-03-29 20:17:58 +0900

    Test: 테스트 환경에서 재사용 가능한 전진 전략 클래스(ForwardNumberGenerator) 생성 및 주입

--- src/test/java/model/RacingGameTest.java
+++ src/test/java/model/RacingGameTest.java
@@ -11,10 +11,9 @@ class RacingGameTest {
     void 자동차들이_무조건_전진하는_가짜_기계로_우승자를_테스트한다() {
         RacingGame game = new RacingGame(Arrays.asList("pobi", "crong"), 1);
 
-        // 람다(가짜 기계): 무조건 9만 반환하도록 조작
-        NumberGenerator fixedGenerator = () -> 9;
+        NumberGenerator forwardGenerator = new ForwardNumberGenerator();
 
-        game.playOneRound(fixedGenerator); // 모두가 9를 받아서 전진함
+        game.playOneRound(forwardGenerator); // 모두가 9를 받아서 전진함
 
         List<Car> winners = game.getWinners();
         assertThat(winners).extracting(Car::getName)
