04639c7b513d161b555905246099057b590fdc2e
haeun92e0 <gracekim8729@gmail.com>
2026-03-29 20:26:24 +0900

    Test: @ValueSource를 활용하여 자동차 정지 조건(0~3)에 대한 파라미터화 테스트 적용

--- src/test/java/model/CarTest.java
+++ src/test/java/model/CarTest.java
@@ -1,25 +1,30 @@
 package model;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 class CarTest {
 
-    @Test
-    void 전달된_값이_4_이상이면_전진한다(){
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    void 전달된_값이_4_이상이면_전진한다(int number) {
         Car car = new Car("pobi");
 
-        car.move(4);
+        car.move(number);
 
         assertThat(car.getPosition()).isEqualTo(1);
     }
 
-    @Test
-    void 전달된_값이_3_이하이면_멈춘다(){
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    void 전달된_값이_3_이하이면_멈춘다(int number) {
         Car car = new Car("rabbi");
 
-        car.move(3);
+        car.move(number);
 
         assertThat(car.getPosition()).isEqualTo(0);
     }
@@ -29,4 +34,5 @@ class CarTest {
         assertThatThrownBy(() -> new Car("abcdef"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
 }
